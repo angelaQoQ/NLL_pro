7c1c7900ed139622fee7bc9b7cb5eb696cd293c3
zhousg <zhoushugang@itcast.cn>
2019-07-09 10:09:54 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 职责1：依赖项目需要的资源（js模块 css资源 ...）
+// 职责2：实例化根实例  render() 渲染APP组件   然后挂载到#app的容器内
 new Vue({
   render: h => h(App)
 }).$mount('#app')
