745a98dfc2fa14e23372b49f1ab817f2d2d0be4e
zhousg <zhoushugang@itcast.cn>
2019-07-09 11:03:10 +0800

    使用element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     App 根组件
+    <el-button type="success">成功按钮</el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 // 职责1：依赖项目需要的资源（js模块 css资源 ...）
